d53b67b01e22a312128e985446a74519c7c5d52c
Snigdha Adhikari <snigdhaadhikari49@gmail.com>
2024-05-15 17:54:35 +0545

    New changes

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,24 +1,4 @@
 const Contact = () => {
-    return <>Contact Us Page;
-    <div className="container">
-        <h1>Login</h1>
-        <form>
-            <div className="username">
-            <input type="text" placeholder="Username" name="" id="" required />
-            </div>
-            <div className="password">
-            <input type="password" placeholder="Password" name="" id="" required/>
-            </div>
-            <div className="rememberPw">
-            <label><input type="checkbox" name="" id="" />Remember me</label>
-            <a href="#">Forgot Password</a>
-            </div>
-            
-            
-
-            <button type="submit">Login</button>
-        </form>
-    </div>
-    </>
+    return <>Contact Us Page</>
 };
 export default Contact;
