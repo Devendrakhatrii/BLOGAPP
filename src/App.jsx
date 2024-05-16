1c9b8d7e4ca2b43a3913ae553e37c5ae1c2eb3a2
Snigdha Adhikari <snigdhaadhikari49@gmail.com>
2024-05-16 14:04:33 +0545

    Display admin and User

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Contact from "./pages/contact";
 import Home from "./pages/home";
+import { About } from "./about";
 
 const App = () => {
   return(
     <>
-   <Contact/>
+   <Contact />
+   <hr/>
    <Home/>
+   <hr/>
+   <About title = "CONTACT INFORMATION" description= "Please enter your valid information" contactno = "ENter your contact number"/>
   </>
  
   ); 
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,31 @@
+import React, { useState } from 'react';
+
 const Contact = () => {
-    return <>Contact Us Page</>
+    const [data, newData] = useState('');
+
+    const Admin = () => {
+        return <p>Welcome Admin</p>;
+    };
+
+    const User = () => {
+        return <p>Welcome User!!!</p>;
+    };
+
+    const updateData = () => {
+        const inputName = document.getElementById("userName").value;
+        const welcomeMessage = inputName === "Snigdha" ? <Admin /> : <User />;
+        newData(welcomeMessage);
+    };  
+
+    return (
+        <>
+            <div className="Contact">
+                <h1>{data}</h1>
+                <input type="text" required id="userName" />
+                <button onClick={updateData}>Click Here</button>
+            </div>
+        </>
+    );
 };
+
 export default Contact;
