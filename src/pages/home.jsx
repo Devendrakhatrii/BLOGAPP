9ffca1a152092426c472c53725d78fab9fb49d78
Snigdha Adhikari <snigdhaadhikari49@gmail.com>
2024-05-18 19:24:06 +0545

    New Data

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,7 @@
 const Home = ({data}) => {
-    <table>
+    return (
+        <>
+<table>
     <thead>
         <tr>
             <th>Name</th>
@@ -7,12 +9,29 @@ const Home = ({data}) => {
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <th></th>
-            <th></th>
-        </tr>
+        {data.length > 0 ? (
+            data.map((user,index) => {
+                return (
+                    <tr key = {index}>
+                    <td>{user.name}</td>
+                    <td>{user.age}</td>
+                </tr>
+                );
+
+            })
+        ):(
+            <tr>
+                <td colSpan = {2} style = {{textAlign:"center"}}>
+                    No Data
+                    </td>
+            </tr>
+        ) }
+
     </tbody>
 </table>
+        </>
+    )
+
     
 }
 export default Home;
