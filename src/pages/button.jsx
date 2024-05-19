241ebd1d359e6d246b5801c34df5cba992447bb6
Snigdha Adhikari <snigdhaadhikari49@gmail.com>
2024-05-19 14:41:46 +0545

    React Hooks practice using Objects

--- src/pages/button.jsx
+++ src/pages/button.jsx
@@ -1,18 +1,25 @@
 import {useState} from 'react'
 
 const Button = () => {
-    const [person, setPerson] = useState('Snigdha'); 
-    const[color,setColor] = useState("red")
+    const [car, setCar] = useState({
+        name: "bmw",
+        model: "black",
+        modelYear: 2012,
+    }); 
+    const updateColor = () => {
+        setCar((prev) => {
+            return {...prev, modelYear: 2010}
+        })
+    }
   return (
 
     <div>
-        person: {person}
-        <input type="text" placeholder='Enter your Name' id ="username" required />
-        <button onClick = {()=>setPerson(document.getElementById('username').value)}>Change</button>
-        Color : {color}
-        <input type="text" placeholder="Enter you color" id="color" />
-        <button onClick = {()=>setColor(document.getElementById('color').value)} >Color</button>
-        
+     {car.name}
+     <button onClick = {() => setCar((prev) => {
+        return {...prev, name: "BMW"};
+     })}>Change Name</button>
+    {car.modelYear}
+     <button onClick = {() => updateColor()}>Change</button>
     </div>
   )
 }
